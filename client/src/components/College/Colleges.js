d0216ccb9bcf03d2417ea4f59945cf3fafd9adec
Jedediah Wood <jedediahwood28@gmail.com>
2021-04-09 16:44:49 -0600

    a lil bit on the college index

--- client/src/components/College/Colleges.js
+++ client/src/components/College/Colleges.js
@@ -1,6 +1,25 @@
-
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Colleges = () => {
+  const [colleges, setColleges] = useState([])
+
+  useEffect(()=>{
+    getColleges()
+  },[])
+
+  const getColleges = async() => {
+    try{
+      let res = await axios.get('/api/colleges')
+    } catch(err){
+      alert('err in getColleges')
+    }
+    return(
+      <>
+
+      </>
+    )
+  }
 
   return(
     <h3>Colleges.js</h3>
