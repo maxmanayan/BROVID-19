a019a6859c1744038acf18049b8c6f09f5bfbea3
Trey Kemp <kemptrey@outlook.com>
2021-04-09 17:12:55 -0600

    Edit Frat form

--- client/src/components/Frats/EditFratForm.js
+++ client/src/components/Frats/EditFratForm.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Button, Form } from 'react-bootstrap'
 
 const editFratForm = () => {
 
   return (
     <div>
-      Lets Party Dude!
+      <h1>Lets Party Dude!</h1>
+    <Form>
+    <Form.Group controlId="formGroupEmail">
+      <Form.Label>Frat Name</Form.Label>
+      <Form.Control type="name" placeholder="Enter Frat Name" />
+    </Form.Group>
+    <Form.Group controlId="formGroupPassword">
+      <Form.Label>Favorite Beer</Form.Label>
+      <Form.Control type="text" placeholder="Enter Favorite Beer" />
+    </Form.Group>
+    <Form.Group controlId="formGroupPassword">
+      <Form.Label>Member Count</Form.Label>
+      <Form.Control type="text" placeholder="How Many Members?" />
+    </Form.Group>
+  </Form>  
+  <Button type='submit'>Submit</Button> 
     </div>
   )
 }
