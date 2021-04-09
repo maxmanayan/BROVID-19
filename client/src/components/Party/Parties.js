7a3cf0354fed1cbd18a5a6c0c54ecd8c6ed206bd
Max Manayan <maximillian521@gmail.com>
2021-04-09 16:35:10 -0600

    parties json rendered on frontend and fixed backgroun

--- client/src/components/Party/Parties.js
+++ client/src/components/Party/Parties.js
@@ -1,9 +1,31 @@
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Parties = () => {
 
+  const [parties, setParties] = useState(null)
+
+
+  useEffect(()=> {
+    getParties()
+  },[])
+
+  const getParties = async () => {
+    try {
+      let res = await axios.get('/api/events')
+      console.log(res.data)
+      setParties(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return(
-    <h3>Parties.js</h3>
+    <>
+      <h3>Parties.js</h3>
+      {parties && <pre>{JSON.stringify(parties, null, 2)}</pre>}
+    </>
+    
   )
 }
 export default Parties;
