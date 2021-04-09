cdf85fa23395c69286ed498344154bdc652f7c75
Jedediah Wood <jedediahwood28@gmail.com>
2021-04-09 16:54:26 -0600

    colledgey show half

--- client/src/components/College/Colleges.js
+++ client/src/components/College/Colleges.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
+import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 
 const Colleges = () => {
   const [colleges, setColleges] = useState([])
@@ -11,14 +12,17 @@ const Colleges = () => {
   const getColleges = async() => {
     try{
       let res = await axios.get('/api/colleges')
+      setColleges(res.data)
+      console.log(res.data)
     } catch(err){
       alert('err in getColleges')
     }
-    return(
-      <>
-
-      </>
-    )
+    
+  }
+  const renderColleges = () => {
+    return Colleges.map( c => {
+      
+    }
   }
 
   return(
