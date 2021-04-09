5964d631a211e476fb80ae0db5536ea16129c9d7
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-04-09 17:18:58 -0600

    frats almost ready

--- client/src/components/Frats/Frats.js
+++ client/src/components/Frats/Frats.js
@@ -1,31 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import useAxiosOnMount from "../../hooks/useAxiosOnMount"
 import {Link} from 'react-router-dom'
 import List from "../List"
 import AxiosContainer from "../AxiosContainer"
+import { Button, Card, Col, Container, Row } from 'react-bootstrap'
+import axios from 'axios'
 
 const Frats = () => {
-  const {data, loading, error} = useAxiosOnMount ('/api/frats')
 
+  const [frats, setFrats] = useState (null)
+
+  useEffect(()=> {
+    getFrats()
+  },[])
+
+  const getFrats = async () => {
+    try {
+      let res = await axios.get('/api/frats')
+      console.log(res.data)
+      setFrats(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const renderFratCards = () => {
+    return frats.map( frat => {
+      return(
+        <Card className="text-center" style={{marginTop: '2em'}}>
+          <Card.Header>{frat.name}</Card.Header>
+          <Card.Body>
+            <Card.Title>Theme: {frat.name}</Card.Title>
+            <Card.Text>
+            Info: {frat.fav_beer}
+            </Card.Text>
+            <Button variant="primary">Checkout Frat</Button>
+          </Card.Body>
+          <Card.Footer className="text-muted">total bro's: {frat.member_count}</Card.Footer>
+        </Card>
+      )
+    })
+  }
   return (
+    <Container>
+    <Row>
+      <Col>
+        <h3>Money Bro</h3>
+      </Col>
 
-    <div> 
-       <AxiosContainer fullError loading={loading} error={error} loaderMessage={'Loading URL, please wait'}>
-        <List name='Frats'
-          data={data}
-          renderData={(frat)=> { 
-          return(
-            <div style={{padding: '5px'}}>
-              <Link to={`/Frats/${frat.id}`}>
-            <p>{frat.name}</p>
-              </Link>
-            </div>
-            )} 
-          }
-        />
+    </Row>
+    {frats && renderFratCards()}
+    {/* {parties && <pre>{JSON.stringify(parties, null, 2)}</pre>} */}
 
-      </AxiosContainer>
-    </div>
+  </Container>
+
   )
 }
 
