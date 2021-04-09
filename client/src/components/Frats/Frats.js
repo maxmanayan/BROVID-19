e1403a0e4869d53f0f1291591726e915a84f1714
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-04-09 16:59:10 -0600

    frats page

--- client/src/components/Frats/Frats.js
+++ client/src/components/Frats/Frats.js
@@ -1,8 +1,32 @@
+import React from 'react'
+import useAxiosOnMount from "../../hooks/useAxiosOnMount"
+import {Link} from 'react-router-dom'
+import List from "../List"
+import AxiosContainer from "../AxiosContainer"
 
 const Frats = () => {
+  const {data, loading, error} = useAxiosOnMount ('/api/frats')
 
-  return(
-    <h3>Frats.js</h3>
+  return (
+
+    <div> 
+       <AxiosContainer fullError loading={loading} error={error} loaderMessage={'Loading URL, please wait'}>
+        <List name='Frats'
+          data={data}
+          renderData={(frat)=> { 
+          return(
+            <div style={{padding: '5px'}}>
+              <Link to={`/Frats/${frat.id}`}>
+            <p>{frat.name}</p>
+              </Link>
+            </div>
+            )} 
+          }
+        />
+
+      </AxiosContainer>
+    </div>
   )
 }
-export default Frats;
+
+export default Frats
