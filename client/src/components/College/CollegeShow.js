99e6069d60b69dc239feef04096c05fb041c872a
Jedediah Wood <jedediahwood28@gmail.com>
2021-04-10 11:50:52 -0600

    still sworking

--- client/src/components/College/CollegeShow.js
+++ client/src/components/College/CollegeShow.js
@@ -1,39 +1,42 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router";
 
 
 const CollegeShow = () => {
   const {id} = useParams()  
   const [college, setCollege] = useState([])
+  const [frat, setFrat] = useState([])
 
   useEffect(()=>{
     singleCollege()
   },[])
 
   const singleCollege = async() => {
+    try{
     let res = await axios.get(`/api/colleges/${id}`)
-    setCollege(res.data)
+    setCollege(res.data.college)
     console.log('--------')
     console.log(res.data)
+    } catch(err){
+      alert('err in singleCollege')
+    }
   }
 
   return(
-    <h2>CollegeShow.js, {id}, </h2>
-    // <>
-    // <Container>
-    //   <Row>
-    //     <Col>
-    //       <h1>Party Schools Near You:</h1>
-    //     </Col>
-
-    //   </Row>
-    //   {college && singleCollege()}
-    //   {/* {parties && <pre>{JSON.stringify(parties, null, 2)}</pre>} */}
-
-    // </Container>
-    // </>
+    // <h2>CollegeShow.js, {id}, </h2>
+    <Card className="text-center" style={{marginTop: '2em'}}>
+          <Card.Header>College:</Card.Header>
+          <Card.Body>
+            <Card.Title>{college.name}</Card.Title>
+            <Card.Text>
+              Mascot: 
+            </Card.Text>
+            <Button>See Parties</Button>
+          </Card.Body>
+          {/* <Card.Footer className="text-muted">{c.party.date} (at the house)</Card.Footer> */}
+        </Card>
   )
 }
 export default CollegeShow;
