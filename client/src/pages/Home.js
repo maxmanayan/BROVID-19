66a92dca0029b509980de7ab55a29a78b841113b
Jedediah Wood <jedediahwood28@gmail.com>
2021-04-10 15:48:17 -0600

    homepage done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -43,17 +43,23 @@ const Home = () => {
       //   <p>Email: {user.email}</p>
       // </Card>
         <Jumbotron>
-          <h1>Hello, world!</h1>
+          <h1>Introducing the new nationwide pandemic of BROVID-19, Bros!</h1>
+          <br/>
             <p>
-              This is a simple hero unit, a simple jumbotron-style component for calling
-              extra attention to featured content or information.
+              In a world where society frowns down on bro-ing down, parties are suddenly illegal, raging is forbidden...
+              We bring to you... BROVID-19 
             </p>
+            <p className="text-muted">
+              The Parties THEY don't want you to know about.
+            </p>
+            {/* <img src='https://lh3.googleusercontent.com/proxy/RyTWt91o9FAez6aTociKW-a-dWoL8OohbnjwEQNqA2Rp7huEfvXQhX5VzDddHiiKGIlou3vzZorB3eNANSMVIttWsyj4072-c2tgBVF9WdJYs7AtCI1CZ3MbSJtDdjq6U3soR4vXz_10mU812Z9kjz5Db6n53A'/> */}
+            <img src='https://64.media.tumblr.com/tumblr_mb1kvdsEam1qjzk4ko2_r1_400.gifv'/>
           <p>
-            <Button variant="primary">Learn more</Button>
+            <Button style={{marginTop: '1em'}} href='./About'>About Us</Button>
           </p>
         </Jumbotron>
       }
-      <Button>Primary</Button>
+      {/* <Button>Primary</Button> */}
     </div>
   )
 }
